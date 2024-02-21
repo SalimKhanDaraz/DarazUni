7d021e4fb172c3996fad8a6586c7437223b22380
salim <Salim@123Daraz>
2024-02-21 16:16:40 +0500

    Fixes of DU Phases 2 changes

--- src/test/java/DarazUniversity/DU_CourseActionPage.java
+++ src/test/java/DarazUniversity/DU_CourseActionPage.java
@@ -15,6 +15,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import static DarazUniversity.DU_Portal.Venture;
+
 public class DU_CourseActionPage {
 
     public DU_CourseActionPage(WebDriver driver) {PageFactory.initElements(driver, this);}
@@ -27,14 +29,27 @@ public class DU_CourseActionPage {
     @FindBy(xpath="//span[@class=\"action-type\"]")
     public WebElement ActionPage_Button;
 
+
+    @FindBy(className="button")
+    public WebElement ActionPageDone_Button;
+
     public void ActionPage (WebDriver driver) throws InterruptedException {
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
         SoftAssert softAssert = new SoftAssert();
 
-        wait.until(ExpectedConditions.visibilityOf(ActionPage_Button)).isDisplayed();
-        ActionPage_Button.click();
+        boolean ActionPage;
+        boolean ActionPageDoneButton;
+
+       // wait.until(ExpectedConditions.visibilityOf(ActionPage_Button)).isDisplayed();
+        //ActionPage_Button.click();
+
+        switch (Venture) {
+
+            case "PK":
+
+        driver.navigate().to("https://university.daraz.pk/course/action?id=5518&cid=5518&sid=942");
         /*
         Set<String> handles = driver.getWindowHandles();
         String currentHandle = driver.getWindowHandle();
@@ -51,7 +66,7 @@ public class DU_CourseActionPage {
         List<String> tabs = new ArrayList<>(driver.getWindowHandles());
          */
 
-        boolean ActionPage;
+
 
         try {
 
@@ -59,12 +74,15 @@ public class DU_CourseActionPage {
             wait.until(ExpectedConditions.visibilityOf(ActionDetailsPage)).isDisplayed();
             ActionPage  = ActionDetailsPage.isDisplayed();
 
-        }catch (Exception e){
+            wait.until(ExpectedConditions.visibilityOf(ActionPageDone_Button)).isDisplayed();
+            ActionPageDoneButton  = ActionPageDone_Button.isDisplayed();
 
+        }catch (Exception e){
             ActionPage = false;
+            ActionPageDoneButton = false;
         }
 
-        if (ActionPage)
+        if (ActionPage && ActionPageDoneButton)
         {
             System.out.println("******************************************");
             System.out.println("Check 1: Action Page Details are available");
@@ -83,13 +101,137 @@ public class DU_CourseActionPage {
 
             softAssert.assertFalse(true,"Action Page Details Failed");
         }
+                break;
+
+            case "LK":
+
+                driver.navigate().to("https://university.daraz.lk/course/action?id=13702&cid=13702&sid=1424");
+
+                try {
+
+                    Thread.sleep(5000);
+                    wait.until(ExpectedConditions.visibilityOf(ActionDetailsPage)).isDisplayed();
+                    ActionPage  = ActionDetailsPage.isDisplayed();
+
+                    wait.until(ExpectedConditions.visibilityOf(ActionPageDone_Button)).isDisplayed();
+                    ActionPageDoneButton  = ActionPageDone_Button.isDisplayed();
+
+                }catch (Exception e){
+                    ActionPage = false;
+                    ActionPageDoneButton = false;
+                }
+
+                if (ActionPage && ActionPageDoneButton)
+                {
+                    System.out.println("******************************************");
+                    System.out.println("Check 1: Action Page Details are available");
+                    Allure.step("Action Page Details are available");
+                    System.out.println("Case: Action Page Details Passed");
+                    System.out.println("********************************");
+                    System.out.println("");
+                }else {
+                    System.out.println("");
+                    System.out.println("****************************************");
+                    System.out.println("Check 2: Action Page Details are missing");
+                    Allure.step("Action Page Details are missing");
+                    System.out.println("Case: Action Page Details Failed");
+                    System.out.println("********************************");
+                    System.out.println("");
+
+                    softAssert.assertFalse(true,"Action Page Details Failed");
+                }
+                break;
+
+            case "BD":
+
+                driver.navigate().to("https://university.daraz.com.bd/course/action?id=13992&cid=13726&sid=1426");
+
+                try {
+
+                    Thread.sleep(5000);
+                    wait.until(ExpectedConditions.visibilityOf(ActionDetailsPage)).isDisplayed();
+                    ActionPage  = ActionDetailsPage.isDisplayed();
+
+                    wait.until(ExpectedConditions.visibilityOf(ActionPageDone_Button)).isDisplayed();
+                    ActionPageDoneButton  = ActionPageDone_Button.isDisplayed();
+
+                }catch (Exception e){
+                    ActionPage = false;
+                    ActionPageDoneButton = false;
+                }
+
+                if (ActionPage && ActionPageDoneButton)
+                {
+                    System.out.println("******************************************");
+                    System.out.println("Check 1: Action Page Details are available");
+                    Allure.step("Action Page Details are available");
+                    System.out.println("Case: Action Page Details Passed");
+                    System.out.println("********************************");
+                    System.out.println("");
+                }else {
+                    System.out.println("");
+                    System.out.println("****************************************");
+                    System.out.println("Check 2: Action Page Details are missing");
+                    Allure.step("Action Page Details are missing");
+                    System.out.println("Case: Action Page Details Failed");
+                    System.out.println("********************************");
+                    System.out.println("");
+
+                    softAssert.assertFalse(true,"Action Page Details Failed");
+                }
+                break;
+
+            case "NP":
+
+                driver.navigate().to("https://university.daraz.com.np/course/action?id=13461&cid=13461&sid=1404");
+
+                try {
+
+                    Thread.sleep(5000);
+                    wait.until(ExpectedConditions.visibilityOf(ActionDetailsPage)).isDisplayed();
+                    ActionPage  = ActionDetailsPage.isDisplayed();
+
+                    wait.until(ExpectedConditions.visibilityOf(ActionPageDone_Button)).isDisplayed();
+                    ActionPageDoneButton  = ActionPageDone_Button.isDisplayed();
+
+                }catch (Exception e){
+                    ActionPage = false;
+                    ActionPageDoneButton = false;
+                }
+
+                if (ActionPage && ActionPageDoneButton)
+                {
+                    System.out.println("******************************************");
+                    System.out.println("Check 1: Action Page Details are available");
+                    Allure.step("Action Page Details are available");
+                    System.out.println("Case: Action Page Details Passed");
+                    System.out.println("********************************");
+                    System.out.println("");
+                }else {
+                    System.out.println("");
+                    System.out.println("****************************************");
+                    System.out.println("Check 2: Action Page Details are missing");
+                    Allure.step("Action Page Details are missing");
+                    System.out.println("Case: Action Page Details Failed");
+                    System.out.println("********************************");
+                    System.out.println("");
+
+                    softAssert.assertFalse(true,"Action Page Details Failed");
+                }
+                break;
+
+
+        }
 
         //driver.close();
+
+        ActionPageDone_Button.click();
+        Thread.sleep(3000);
+
         softAssert.assertAll();
 
         //driver.switchTo().window(tabs.get(0));
-
-        driver.navigate().back();
+        //driver.navigate().back();
     }
 
 }
--- src/test/java/DarazUniversity/DU_CourseDetailsPage.java
+++ src/test/java/DarazUniversity/DU_CourseDetailsPage.java
@@ -47,11 +47,20 @@ public class DU_CourseDetailsPage {
     @FindBy(xpath="//*[text()=\"Courses\"]")
     public WebElement Courses_Filter;
 
-    @FindBy(xpath="//a[text()=\"How to Grow Traffic On Your Store\"]")
+    @FindBy(xpath="//a[text()=\"All About Fulfilment by Daraz\"]")
     public WebElement Course;
 
     @FindBy(xpath="//div[@class=\"recommend \"]//a[text()=\"Content Library\"]")
     public WebElement ContentLibrary;
+
+    @FindBy(xpath="//div[@class=\"progress-bar-full\"]")
+    public WebElement ProgressBar_CourseDetailsPage;
+
+    @FindBy(xpath="//span[@class=\"percent\"]")
+    public WebElement ProgressValue_CourseDetailsPage;
+
+    @FindBy(xpath="//a[@class=\"course-button outline\"]")
+    public WebElement Continue_Button;
 
     public void CourseDetailsPage (WebDriver driver) throws InterruptedException {
 
@@ -82,7 +91,7 @@ public class DU_CourseDetailsPage {
                     Course.click();
 
                 }catch (Exception e){
-                    driver.navigate().to("https://university.daraz.lk/course/detail?spm=du-lk-pc.du-lk-pc-list.courserlist_courses.9.28c34984juLWMD&id=1430&type=series");
+                    driver.navigate().to("https://university.daraz.pk/course/detail?spm=du-pk-pc.du-pk-pc-list.courserlist_courses.7.3c604984b1J3Xs&id=1365&type=series");
                     Allure.step("Clicked on How To Grow Traffic On Your Store Course");
                 }
 
@@ -109,7 +118,7 @@ public class DU_CourseDetailsPage {
                     Course.click();
 
                 }catch (Exception e){
-                    driver.navigate().to("https://university.daraz.lk/course/detail?spm=du-lk-pc.du-lk-pc-list.courserlist_courses.9.28c34984juLWMD&id=1430&type=series");
+                    driver.navigate().to("https://university.daraz.lk/course/detail?spm=du-lk-pc.du-lk-pc-list.courserlist_courses.3.232d4984oPFaGQ&id=1504&type=series");
                     Allure.step("Clicked on How To Grow Traffic On Your Store Course");
                 }
 
@@ -163,7 +172,7 @@ public class DU_CourseDetailsPage {
                     Course.click();
 
                 }catch (Exception e){
-                    driver.navigate().to("https://university.daraz.com.bd/course/detail?spm=du-bd-pc.du-bd-pc-list.courserlist_courses.3.49044984FztZIe&id=1407&type=series");
+                    driver.navigate().to("https://university.daraz.com.bd/course/detail?spm=du-bd-pc.du-bd-pc-list.courserlist_courses.1.77104984TpzLh0&id=1164&type=series");
                     Allure.step("Clicked on How To Grow Traffic On Your Store Course");
                 }
 
@@ -273,21 +282,21 @@ public class DU_CourseDetailsPage {
             System.out.println("");
 
             softAssert.assertFalse(true,"Course Detail Page Failed");
-
         }
 
         js.executeScript("arguments[0].scrollIntoView();", StartCourse_Button);
         Thread.sleep(3000);
 
         softAssert.assertAll();
-
     }
 
     public void CourseStatistics (WebDriver driver) throws InterruptedException {
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
-        wait.until(ExpectedConditions.visibilityOfAllElements(CourseInfo));
+        //wait.until(ExpectedConditions.visibilityOfAllElements(CourseInfo));
+
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@class=\"course-info-text th-ff1 th-fs9\"]")));
 
         SoftAssert softAssert = new SoftAssert();
 
@@ -306,5 +315,148 @@ public class DU_CourseDetailsPage {
 
         softAssert.assertAll();
 
+    }
+
+
+    public void CourseDetailsWhenCourseStarted (WebDriver driver){
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        SoftAssert softAssert = new SoftAssert();
+
+        boolean ProgressBar;
+        boolean ProgressBarValue;
+        boolean Continue;
+
+
+        switch (Venture) {
+            case "PK":
+
+                driver.navigate().to("https://university.daraz.pk/course/detail?spm=du-pk-pc.du-pk-pc-list.courserlist_courses.1.55eb4984bHmrxn&id=1350&type=series");
+
+                try{
+
+                    ProgressBar = wait.until(ExpectedConditions.visibilityOf(ProgressBar_CourseDetailsPage)).isDisplayed();
+                    ProgressBarValue = wait.until(ExpectedConditions.visibilityOf(ProgressValue_CourseDetailsPage)).isDisplayed();
+                    Continue = wait.until(ExpectedConditions.visibilityOf(Continue_Button)).isDisplayed();
+
+                }catch (Exception e){
+
+                    ProgressBar = false;
+                    ProgressBarValue = false;
+                    Continue = false;
+                }
+
+                if (ProgressBar || ProgressBarValue || Continue)
+                {
+                    System.out.println("***********************************************");
+                    System.out.println("Check 1: Course Detail Components are available");
+                    Allure.step("Course Detail Components are available");
+                    System.out.println("Case: Course Detail Page Passed");
+                    System.out.println("*******************************");
+                    System.out.println("");
+                }
+                else
+                {
+                    System.out.println("");
+                    System.out.println("*********************************************");
+                    System.out.println("Check 2: Course Detail Components are missing");
+                    Allure.step("Course Detail Components are missing");
+                    System.out.println("Case: Course Detail Page Failed");
+                    System.out.println("**************************************");
+                    System.out.println("");
+
+                    softAssert.assertFalse(true,"Course Detail Page Failed");
+
+                }
+
+                break;
+
+            case "LK":
+
+                driver.navigate().to("https://university.daraz.lk/course/detail?spm=du-lk-pc.du-lk-pc-list.courserlist_courses.13.5dee4984jXCa7B&id=1424&type=series");
+
+                try{
+
+                    ProgressBar = wait.until(ExpectedConditions.visibilityOf(ProgressBar_CourseDetailsPage)).isDisplayed();
+                    ProgressBarValue = wait.until(ExpectedConditions.visibilityOf(ProgressValue_CourseDetailsPage)).isDisplayed();
+                    Continue = wait.until(ExpectedConditions.visibilityOf(Continue_Button)).isDisplayed();
+
+                }catch (Exception e){
+
+                    ProgressBar = false;
+                    ProgressBarValue = false;
+                    Continue = false;
+                }
+
+                if (ProgressBar || ProgressBarValue || Continue)
+                {
+                    System.out.println("***********************************************");
+                    System.out.println("Check 1: Course Detail Components are available");
+                    Allure.step("Course Detail Components are available");
+                    System.out.println("Case: Course Detail Page Passed");
+                    System.out.println("*******************************");
+                    System.out.println("");
+                }
+                else
+                {
+                    System.out.println("");
+                    System.out.println("*********************************************");
+                    System.out.println("Check 2: Course Detail Components are missing");
+                    Allure.step("Course Detail Components are missing");
+                    System.out.println("Case: Course Detail Page Failed");
+                    System.out.println("**************************************");
+                    System.out.println("");
+
+                    softAssert.assertFalse(true,"Course Detail Page Failed");
+
+                }
+
+                break;
+
+            case "BD":
+
+                driver.navigate().to("https://university.daraz.com.bd/course/detail?spm=du-bd-pc.du-bd-pc-list.courserlist_courses.5.12ef4984kjeRoq&id=1426&type=series");
+
+                try{
+
+                    ProgressBar = wait.until(ExpectedConditions.visibilityOf(ProgressBar_CourseDetailsPage)).isDisplayed();
+                    ProgressBarValue = wait.until(ExpectedConditions.visibilityOf(ProgressValue_CourseDetailsPage)).isDisplayed();
+                    Continue = wait.until(ExpectedConditions.visibilityOf(Continue_Button)).isDisplayed();
+
+                }catch (Exception e){
+
+                    ProgressBar = false;
+                    ProgressBarValue = false;
+                    Continue = false;
+                }
+
+                if (ProgressBar || ProgressBarValue || Continue)
+                {
+                    System.out.println("***********************************************");
+                    System.out.println("Check 1: Course Detail Components are available");
+                    Allure.step("Course Detail Components are available");
+                    System.out.println("Case: Course Detail Page Passed");
+                    System.out.println("*******************************");
+                    System.out.println("");
+                }
+                else
+                {
+                    System.out.println("");
+                    System.out.println("*********************************************");
+                    System.out.println("Check 2: Course Detail Components are missing");
+                    Allure.step("Course Detail Components are missing");
+                    System.out.println("Case: Course Detail Page Failed");
+                    System.out.println("**************************************");
+                    System.out.println("");
+
+                    softAssert.assertFalse(true,"Course Detail Page Failed");
+
+                }
+
+                break;
+
+       }
+        softAssert.assertAll();
     }
 }
